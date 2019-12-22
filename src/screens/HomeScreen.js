7b367c2ee78b20db307b06db15f275e5f9890a0e
googolaire <thehuntermaxwell@gmail.com>
2019-12-21 19:16:01 -0600

    Day one commit 005

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, StyleSheet, TextInput } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <>
-  <Text style={styles.text}>instaEStore</Text>
+  <Text style={styles.text}>Day One React Native</Text>
+  <Button 
+  title="Go to the Components Screen"
+  onPress={() => console.log('Button Pressed')}
+  />
+  <TouchableOpacity onPress={()=>console.log('List Pressed')}>
+    <Text>Go to List Demo</Text>
+  </TouchableOpacity>
     </>
     );
 };
