68ed1e323968ef2af29374d78b80517904269541
googolaire <thehuntermaxwell@gmail.com>
2019-12-21 18:40:14 -0600

    Day one commit 003

--- src/screens/components/ListScreen.js
+++ src/screens/components/ListScreen.js
@@ -4,17 +4,21 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
     const friends = [
-        {name: 'Friend #1', key: '1'},
-        {name: 'Friend #2', key: '2'},
-        {name: 'Friend #3', key: '3'},
-        {name: 'Friend #4', key: '4'},
-        {name: 'Friend #5', key: '5'},
-        {name: 'Friend #6', key: '6'},
-        {name: 'Friend #7', key: '7'}
+        {name: 'Friend #1'},
+        {name: 'Friend #2'},
+        {name: 'Friend #3'},
+        {name: 'Friend #4'},
+        {name: 'Friend #5'},
+        {name: 'Friend #6'},
+        {name: 'Friend #7'},
+        {name: 'Friend #8'},
+        {name: 'Friend #9'},
     ];
     return (
     <FlatList
-    keyExtractor={(friends) => friends.name} 
+    horizontal
+    keyExtractor={(friends) => friends.name}
+    showsHorizontalScrollIndicator={false} 
     data={friends} 
     renderItem={({item}) => {
     return <Text style={styles.textStyle}>{item.name}</Text>
